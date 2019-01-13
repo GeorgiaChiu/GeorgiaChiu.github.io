b3807663f6959b83814fdddce92ece2814018ce4
Georgiachiu <ccg31112@gmail.com>
2019-01-13 13:54:14 +0800

    reborn

---
+++ js/index-tra00.js
@@ -0,0 +1,124 @@
+var posts = [
+    {
+      id: 1,
+      title: "【 花蓮 】",
+      date: "Hualien",
+      description: "<p>2016.Jun-Jul</p><p>這個夏天改變我很多，我還是很喜歡那裡得山水，非常非常想念。</p><li>旅遊方式：打工換宿</li><li>找到辦法：facebook 打工換宿社團</li><li>需帶物品：一顆快樂的心</li><li>食：食花蓮好空氣</li><li>衣：著花蓮好山水</li><li>住：繁華青年旅社</li><li>行：便車, 火車, 走路</li><li>注意事項：打工換宿請要負起一定的責任，但遇到惡雇主請也要勇敢說不。</li><p></p><a href='index-notyet.html' class='btn' target'_b'>Click me to see more...</a></p>",
+      image: "../hualien02.jpg",
+      clip_path: "polygon(50% 0%, 90% 30%, 90% 100%, 0% 50%)" },
+    {
+      id: 2,
+      title: "【 日本 】",
+      date: "Japan",
+      description: "<p>去多滿多次日本的，也用過滿多不一樣的身份，只能說，日本真的是超級適合旅遊得啦！</p><li>旅遊方式：純粹旅遊</li><li>需帶物品：網路卡主要翻譯用</li><li>食：天堂</li><li>衣：看地區和天氣</li><li>住：青年旅社</li><li>行：公車, 地鐵, 腳踏車（超好騎）, 走路</li><li>注意事項：日本就是那麼適合旅遊，我也不知道要注意什麼</li><p></p><a href='index-notyet.html' class='btn' target'_b'>Click me to see more...</a></p>",
+      image: "../resourse/japan.jpg",
+      clip_path: "polygon(0% 53%, 90% 30%, 90% 100%, 0% 50%)" },
+    
+    {
+      id: 3,
+      title: "【 加拿大 x 溫哥華島 】",
+      date: "Canada-Vancouver Island",
+      description: "<p>2017.Jun-Jul</p><li>旅遊方式：打工換宿</li><li>找到辦法：helpX</li><li>需帶物品：是地區而定，但請一定要帶乳霜等保濕</li><li>食：帶著每天吃沙拉的心情，在去之前好好吃頓熱炒，儲存想念。/ 可是若可以自己煮，其實物價也不會太貴，美西的酪梨十分便宜。</li><li>衣：除非是7-8歲的酷暑，不然萬分建議帶著大外套，當時去的六月，沒有陽光都會冷到打哆嗦那種。</li><li>住：因為是打工換宿，我住過Airbnb, 開發中農場, 當地朋友家，下一張網頁會跟你們慢慢分享：） </li><li>行：機票最便宜可以壓到10000來回（聽朋友說），我當時買約20000來回（轉機一次，北京）當地加拿大交通費頗貴，覺得跟台灣客運比起來，大概是1.5倍左右。加拿大建設優良，只要有網路就不會迷路，而且當地人都非常熱心，幾乎都會給予支援。</li><li>注意事項：治安好，機能方便，覺得沒什麼特別需要留意的地方。</li><p></p><a href='index-notyet.html' class='btn' target'_b'>Click me to see more...</a></p>",
+      image: "../resourse/canada.jpg",
+      clip_path: "polygon(10% 13%, 90% 30%, 50% 100%, 10% 10%)" },
+    
+    {
+      id: 4,
+      title: "【 爬山 】",
+      date: "Hiking",
+      description: "<p>很喜歡山，真的很喜歡。<p><li>旅遊方式：純粹旅遊</li><li>找到辦法：靠朋友</li><li>需帶物品：一顆建勇的心</li><li>食：飢不擇食，爬完山在帳棚過夜野炊時，就算食物掉到地板也直接賽進嘴吧。但還是要頻估好份量，不要再把食物帶下山，可以為自己省去些體力唷～</li><li>衣：保暖衣物絕對不能少，發熱衣和套頭是必備，而每趟都要準備更換的衣物，因為爬山一定爆汗，讓身體保持乾燥是非常重要的事。再來，請一定多多多帶很多雙襪子！腳能乾爽的走路，絕對會幫助自己遠離苦痛啊！</li><li>住：帳篷</li><li>行：通常會找旅行社包車，但廢廢的我，都是周遭朋友訂車，沒訂過不知道如何。</li><p></p><a href='index-notyet.html' class='btn' target'_b'>Click me to see more...</a></p>",
+      image: "../resourse/introme.jpg",
+      clip_path: "polygon(0 20%, 100% 0, 100% 70%, 48% 100%)" },
+      {
+      id: 5,
+      title: "【 南美洲 】",
+      date: "Southern America",
+      description: "<p>2018.Jul-Aug</p><p>哥倫比亞 厄瓜多 秘魯</p><p>奇幻的南美洲之旅，酸甜苦辣如人生<p><li>旅遊方式：純粹旅遊</li><li>找到辦法：靠自己</li><li>需帶物品：視地區而定：但南美洲的大城市基本都是在高山上，所以一年四季都是冷的，所以還是不能忘記帶保暖衣物喔。南美洲其實很冷得。</li><li>食：南美洲的食物很讚！又便宜！可是喜歡吃泡麵類型的人要注意，當地的泡麵種類少得可憐。米食十分常見，在秘魯還有炒飯可以吃，在當地可以看到非常多“Chifa“的招牌，雖然沒人能證實，但我覺得一定跟我們的”吃飯“有關連。而飯很鹹，澱粉豆類非常之多，一餐的份量像是台灣的三份。結論：在南美洲不可能餓死。（大家還會塞東西給你吃）</li><li>衣：有想去馬丘比丘得人，一定要帶保暖衣物，晚間和黎明都可以到1,2度低溫，超級超級冷，請格外注意。而大城市，基本上也都滿濕冷得，雨具和保暖請不要輕忽。但如果是去海邊，拜託把他們全部仍掉，你會熱死。</li><li>住：青年旅社，而且南美青旅非常便宜，台幣150-300的區間是很正常的價格，大部分也供應廚房和些簡單熱飲，然後旅人都愛煮飯，在餐廳晃一晃交朋友大概是旅行的真諦之一吧。唯一要注意是，南美洲的熱水供應很不穩定，請大家洗澡不要太晚，不然幾乎都會沒有熱水，這點請萬分銘記，因為小女有切身之痛，你絕對不會想再2度低溫裡洗冷水澡的。（所以就得臭一天了:) 飲用水（是不是打開水龍頭就可以喝水）的部分，哥倫比雅-波哥大,Medellin可以直接飲用；秘魯不行。而建議在旅行時，問問青旅工作人員當地的飲水狀況，問完還是自己張大眼看看能不能喝。有過經驗是，他們說可以，但我想騙過自己那層油和雜質不存在也不行，因為他們真得不太能喝啊…...</li><li>行：機票：最便宜看過35000到波哥大（哥倫比雅）當時買48000（轉機兩次，香港，洛杉磯）Avianca很推，做起來很舒適，服務和艙內設施都很不錯，南美航線得好選擇。/公路之旅：這我一定要跟大家好好解析，因為南美洲的行政系統太隨性了，在這邊無法一一說明，請往裡面點，我會再跟你做詳細介紹，讓你在南美洲不會被氣死的那麼快哈哈！</li><li>注意事項：南美洲治安動盪，走在路上還是會被搶得，建議大家穿的破舊一點，保持機靈喔！</li><p></p><a href='index-tra02-5.html' class='btn' target'_b'>Click me to see more...</a></p>",
+      image: "../resourse/southamerica02.jpg",
+      clip_path: "polygon(0 10%, 90% 10%, 90% 60%, 55% 100%)" },
+    
+    
+    
+    // {
+    //   id: 4,
+    //   title: ":: 文字作品 ::",
+    //   date: "Literary Creation",
+    //   description: "<p>hi</p><p></p><a href='https://image.ibb.co/bF9iO5/1.jpg'>fu06j;6</a>",
+    //   image: "../resourse/lit.jpg",
+    //   clip_path: "polygon(0 100%, 46% 0, 100% 70%, 49% 69%)" }
+    ];
+    
+    
+    
+    var app = new Vue({
+      el: "#app",
+      data: function data() {
+        return {
+          test: "",
+          posts: posts,
+          selectedClipPath: "polygon(0 100%, 0 0, 100% 0, 100% 100%)",
+          postImage: posts[0].image,
+          currentPost: null,
+          postIndex: 0,
+          selected: false,
+          ready: false,
+          lastSelectedPost: null,
+          clipPath: posts[0].clip_path };
+    
+      },
+      methods: {
+        changePost: function changePost(index) {
+          if (this.postImage != this.posts[index].image && this.selected == false) {
+            this.postImage = this.posts[index].image;
+            this.clipPath = this.posts[index].clip_path;
+          }
+        },
+        closePost: function closePost() {
+          if (this.lastSelectedPost != null) {
+            this.selected = false;
+            this.ready = false;
+            this.clipPath = this.posts[this.lastSelectedPost].clip_path;
+          }
+        },
+        selectedPost: function selectedPost(index) {var _this = this;
+          this.selected = true;
+          this.clipPath = this.selectedClipPath;
+          this.lastSelectedPost = index;
+          this.currentPost = this.posts[index];
+          setTimeout(function () {
+            _this.ready = true;
+          }, 600);
+        },
+        nextPost: function nextPost() {var _this2 = this;
+          if (this.lastSelectedPost < this.posts.length - 1) {
+            this.lastSelectedPost++;
+            this.ready = false;
+            setTimeout(function () {
+              _this2.currentPost = _this2.posts[_this2.lastSelectedPost];
+              _this2.postImage = _this2.currentPost.image;
+              _this2.ready = true;
+            }, 600);
+          }
+        },
+        prevPost: function prevPost() {var _this3 = this;
+          if (
+          this.lastSelectedPost <= this.posts.length - 1 &&
+          this.lastSelectedPost != 0)
+          {
+            this.lastSelectedPost--;
+            this.ready = false;
+            setTimeout(function () {
+              _this3.currentPost = _this3.posts[_this3.lastSelectedPost];
+              _this3.postImage = _this3.currentPost.image;
+              _this3.ready = true;
+            }, 600);
+          }
+        } },
+    
+      created: function created() {var _this4 = this;
+        window.addEventListener("keydown", function (e) {
+          e.keyCode == 39 ? _this4.nextPost() : false;
+          e.keyCode == 37 ? _this4.prevPost() : false;
+          e.keyCode == 27 ? _this4.closePost() : false;
+        });
+      } });
