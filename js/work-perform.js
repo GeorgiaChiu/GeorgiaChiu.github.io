29e3d1a19af95df206f67f3db6cd780dfc72015a
GeorgiaChiu <42746780+GeorgiaChiu@users.noreply.github.com>
2020-04-23 21:58:03 +0800

    capital

---
+++ js/work-perform.js
@@ -0,0 +1,30 @@
+// set images
+
+
+var images = [
+    "/resourse/intro01-2.JPG",
+    "/resourse/works/visuals/visual04.png",
+    "/resourse/energy-1.jpg",
+    "/resourse/works/performance/201906-3.jpg",
+    "/resourse/works/performance/201906-7-1.jpg",
+  ]
+  // instanciate slider
+  var rbgShiftSlider = new rbgShiftSlider({
+    // nav : true,
+    // navElement: '.scene-nav',
+    slideImages: images,
+    stageWidth: 1920,
+    stageHeight: 1080,
+    displacementImage: 'http://hmongouachon.com/_demos/rgbShiftSlider/displace-circle.png',
+    fullScreen: true,
+    transitionDuration: 0.55, // must be 0.1 > transitionGhostDuration
+    transitionGhostDuration : 0.15,
+    transitionFilterIntensity: 350,
+    transitionSpriteIntensity: 2,
+    mouseDispIntensity: 3,
+    interactive : true,
+    autoPlay : true,
+    autoPlaySpeed : 3000,
+
+    
+  });
