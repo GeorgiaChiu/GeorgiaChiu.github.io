bec690f405c42e3c23a8ec66f0e63921ff55854a
GeorgiaChiu <42746780+GeorgiaChiu@users.noreply.github.com>
2020-06-02 00:45:35 +0800

    lazi

---
+++ js/script-lazi2.js
@@ -0,0 +1,32 @@
+// set images
+
+
+var images = [
+    "/resourse//works/performance/201906-6-1.jpg",
+    "/resourse/works/performance/201807-1-1.jpg",
+    "/resourse/works/visuals/visual04.jpg",
+    "/resourse/energy-1.jpg",
+    "/resourse/works/performance/201906-3.jpg",
+    "/resourse/works/performance/201906-7-1.jpg",
+  
+  ]
+  // instanciate slider
+  var rbgShiftSlider = new rbgShiftSlider({
+    // nav : true,
+    // navElement: '.scene-nav',
+    slideImages: images,
+    stageWidth: 1280,
+    stageHeight: 720,
+    displacementImage: 'http://hmongouachon.com/_demos/rgbShiftSlider/displace-circle.png',
+    fullScreen: true,
+    transitionDuration: 0.55, // must be 0.1 > transitionGhostDuration
+    transitionGhostDuration : 0.15,
+    transitionFilterIntensity: 350,
+    transitionSpriteIntensity: 2,
+    mouseDispIntensity: 3,
+    interactive : true,
+    autoPlay : true,
+    autoPlaySpeed : 3000,
+
+    
+  });
